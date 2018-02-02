29a96c6f71417cd66146f65e1e63b037913599a8
jianlins <jianlinshi.cn@gmail.com>
2018-02-01 21:15:37 -0700

    Chinese simple tokenizer passed.

--- src/test/java/edu/utah/bmi/nlp/RuSH/TestRuSHCN_AE.java
+++ src/test/java/edu/utah/bmi/nlp/RuSH/TestRuSHCN_AE.java
@@ -19,6 +19,7 @@ import edu.utah.bmi.nlp.rush.uima.RuSHTest_AE;
 import edu.utah.bmi.nlp.rush.uima.RuSH_AE;
 import edu.utah.bmi.nlp.type.system.ConceptBASE;
 import edu.utah.bmi.nlp.type.system.SectionBody;
+import edu.utah.bmi.nlp.type.system.Token;
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
@@ -26,11 +27,15 @@ import org.apache.uima.cas.FSIterator;
 import org.apache.uima.examples.SourceDocumentInformation;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * @Author Jianlin Shi
@@ -51,7 +56,6 @@ public class TestRuSHCN_AE {
 				RuSH_AE.PARAM_SENTENCE_TYPE_NAME, "Sentence",
 				RuSH_AE.PARAM_TOKEN_TYPE_NAME, "Token",
 				RuSH_AE.PARAM_RULE_STR, rule,
-				RuSH_AE.PARAM_INCLUDE_PUNCTUATION,true,
 				RuSH_AE.PARAM_LANGUAGE,"cn",
 				RuSH_AE.PARAM_FIX_GAPS, true);
 		testAnalysisEngine = AnalysisEngineFactory.createEngine(
@@ -69,18 +73,62 @@ public class TestRuSHCN_AE {
 		new SourceDocumentInformation(jCas,0,input.length()).addToIndexes();
 		analysisEngine.process(jCas);
 		printAnnotations(jCas);
-//		testAnalysisEngine.process(jCas);
+		Collection<Token> res = JCasUtil.select(jCas, Token.class);
+		ArrayList<Token> tokens=new ArrayList<>();
+		tokens.addAll(res);
+		assert (tokens.get(0).getCoveredText().equals("患"));
+		assert (tokens.get(1).getCoveredText().equals("者"));
+		assert (tokens.get(2).getCoveredText().equals("血"));
+		assert (tokens.get(3).getCoveredText().equals("压"));
+		assert (tokens.get(4).getCoveredText().equals("123"));
+		assert (tokens.get(5).getCoveredText().equals("88"));
+		assert (tokens.get(6).getCoveredText().equals("mmHg"));
+		assert (tokens.get(7).getCoveredText().equals("呼"));
+		assert (tokens.get(8).getCoveredText().equals("吸"));
+		assert (tokens.get(9).getCoveredText().equals("3.0"));
+		assert (tokens.get(10).getCoveredText().equals("次"));
+		assert (tokens.get(11).getCoveredText().equals("分"));
 	}
 
 	@Test
-	public void test2() throws AnalysisEngineProcessException {
+	public void test2() throws AnalysisEngineProcessException, ResourceInitializationException {
 		String input="患者血压123/88mmHg，呼吸3.0次/分。";
 		jCas.reset();
 		jCas.setDocumentText(input);
 		new SourceDocumentInformation(jCas,0,input.length()).addToIndexes();
+		String rule="@fastcnercn\n" +
+				"\\b(\\a\t0\tstbegin\n" +
+				"\\a\\e\t2\tstend\n" ;
+		analysisEngine = AnalysisEngineFactory.createEngine(
+				RuSH_AE.class,
+				RuSH_AE.PARAM_SENTENCE_TYPE_NAME, "Sentence",
+				RuSH_AE.PARAM_TOKEN_TYPE_NAME, "Token",
+				RuSH_AE.PARAM_RULE_STR, rule,
+				RuSH_AE.PARAM_INCLUDE_PUNCTUATION,true,
+				RuSH_AE.PARAM_LANGUAGE,"cn",
+				RuSH_AE.PARAM_FIX_GAPS, true);
 		analysisEngine.process(jCas);
-		printAnnotations(jCas);
-//		testAnalysisEngine.process(jCas);
+		Collection<Token> res = JCasUtil.select(jCas, Token.class);
+		ArrayList<Token> tokens=new ArrayList<>();
+		tokens.addAll(res);
+
+		assert (tokens.get(0).getCoveredText().equals("患"));
+		assert (tokens.get(1).getCoveredText().equals("者"));
+		assert (tokens.get(2).getCoveredText().equals("血"));
+		assert (tokens.get(3).getCoveredText().equals("压"));
+		assert (tokens.get(4).getCoveredText().equals("123"));
+		assert (tokens.get(5).getCoveredText().equals("/"));
+		assert (tokens.get(6).getCoveredText().equals("88"));
+		assert (tokens.get(7).getCoveredText().equals("mmHg"));
+		assert (tokens.get(8).getCoveredText().equals("，"));
+		assert (tokens.get(9).getCoveredText().equals("呼"));
+		assert (tokens.get(10).getCoveredText().equals("吸"));
+		assert (tokens.get(11).getCoveredText().equals("3.0"));
+		assert (tokens.get(12).getCoveredText().equals("次"));
+		assert (tokens.get(13).getCoveredText().equals("/"));
+		assert (tokens.get(14).getCoveredText().equals("分"));
+		assert (tokens.get(15).getCoveredText().equals("。"));
+
 	}
 
 	private void printAnnotations(JCas jCas) {
@@ -90,6 +138,12 @@ public class TestRuSHCN_AE {
 			System.out.println(thisAnnotation.getClass().getSimpleName());
 			System.out.println("\t" + thisAnnotation.getCoveredText());
 			System.out.println("\t" + thisAnnotation.getBegin()+"-" + thisAnnotation.getEnd());
+		}
+		Collection<Token> res = JCasUtil.select(jCas, Token.class);
+		ArrayList<Token> tokens=new ArrayList<>();
+		tokens.addAll(res);
+		for(int i=0;i<tokens.size();i++){
+			System.out.println("assert (tokens.get("+i+").getCoveredText().equals(\""+tokens.get(i).getCoveredText()+"\"));");
 		}
 	}
 }
