696ff491056565e4d11817e432a675b8e63be134
jianlins <jianlinshi.cn@gmail.com>
2021-10-11 05:18:31 -0600

    all tests passed

--- src/main/java/edu/utah/bmi/nlp/fastcner/FastRuSHRule_HCN.java
+++ src/main/java/edu/utah/bmi/nlp/fastcner/FastRuSHRule_HCN.java
@@ -38,7 +38,6 @@ public class FastRuSHRule_HCN extends FastRuSHRule_H {
     }
 
 
-
     protected boolean iss(char thisChar) {
         return (thisChar == ' ' || thisChar == '\t' || (int) thisChar == 160);
     }
@@ -46,6 +45,7 @@ public class FastRuSHRule_HCN extends FastRuSHRule_H {
     protected boolean isd(char thisChar) {
         return UnicodeChecker.isDigit(thisChar);
     }
+
     protected boolean isC(char thisChar) {
         return UnicodeChecker.isChinese(thisChar);
     }
@@ -70,5 +70,95 @@ public class FastRuSHRule_HCN extends FastRuSHRule_H {
         return !isWhitespace(thisChar) && (int) thisChar != 160 && thisChar != '　';
     }
 
+
+//    /**
+//     * Redesigned addDeterminants for RuSH purpose
+//     *
+//     * @param rule            rule Map for processsing
+//     * @param matches         matched results
+//     * @param matchBegin      fbegin position of matched string
+//     * @param matchEnd        fend  position of matched string
+//     * @param currentPosition current position
+//     * @param text            full text
+//     */
+//    protected void addDeterminants(String text, HashMap rule, Map<String, HashMap<Float, Marker>> matches, int ruleStartPosition,
+//                                   int matchBegin, int matchEnd, int currentPosition) {
+//
+//        HashMap<DeterminantValueSet.Determinants, Integer> deterRule = (HashMap<DeterminantValueSet.Determinants, Integer>) rule.get(END);
+//        int end = matchEnd == 0 ? currentPosition : matchEnd;
+////      rule  error detection
+//        if (matchBegin > end) {
+//            StringBuilder sb = new StringBuilder();
+//            for (Object key : deterRule.keySet()) {
+//                int rulePos = deterRule.get(key);
+//                sb.append(getRule(rulePos).toString());
+//                sb.append("\n");
+//            }
+//            logger.warning("Rule definition error ----matched fbegin > matched fend\n" +
+//                    "check the following rules: \n" + sb.toString());
+//            int snippetBegin = matchBegin - 100;
+//            snippetBegin = snippetBegin < 0 ? 0 : snippetBegin;
+//            int snippetEnd = end + 100;
+//            snippetEnd = snippetEnd > text.length() ? text.length() : snippetEnd;
+//            logger.warning("try to match span: " + text.substring(snippetBegin, end) + "<*>"
+//                    + text.substring(end, matchBegin) + "<*>" + text.substring(matchBegin, snippetEnd));
+//
+//        }
+//        int width = end - matchBegin;
+//        float begin = matchBegin + offset;
+//        end += offset;
+//        Marker currentMarker;
+//
+//        if (logger.isLoggable(Level.FINEST))
+//            logger.finest("Try to addDeterminants: " + begin + ", " + end
+//                    + "\t" + text.substring(Math.round(begin), Math.round(end)));
+//
+//
+//        for (Object key : deterRule.keySet()) {
+//            currentMarker = new Marker(begin, end, currentPosition - ruleStartPosition);
+//            HashMap<Float, Marker> matchedSpans = new HashMap<>();
+//            int rulePos = deterRule.get(key);
+//            Rule matchedRule = this.ruleStore.get(rulePos);
+//            double score = matchedRule.score;
+//            String type = (String) key;
+//            currentMarker.ruleId = rulePos;
+//            currentMarker.score = score;
+//            if (type.endsWith("end")) {
+////                currentMarker.position += 0.6f;
+//                currentMarker.fbegin = currentMarker.fend - 0.4f;
+//                currentMarker.type = MARKERTYPE.END;
+//                if (!typeMergeMap.containsKey(type))
+//                    typeMergeMap.put(type, type.substring(0, type.length() - 3));
+//                type = typeMergeMap.get(type);
+//            } else {
+////               assume the type is "xxxbegin";
+//                currentMarker.type = MARKERTYPE.BEGIN;
+//                if (!typeMergeMap.containsKey(type))
+//                    typeMergeMap.put(type, type.substring(0, type.length() - 5));
+//                type = typeMergeMap.get(type);
+//            }
+//            currentMarker.ruleId = rulePos;
+//            currentMarker.score = score;
+//            if (logger.isLoggable(Level.FINEST))
+//                logger.finest("\t\tRule Id: " + rulePos + "\t" + key + "\t" + getRule(rulePos).type + "\t" + getRuleString(rulePos));
+////          If needed, implement your own selection ruleStore and score updating logic below
+//            if (matches.containsKey(type)) {
+////              because the ruleStore are all processed at the same time from the input left to the input right,
+////                it becomes more efficient to compare the overlaps
+//                matchedSpans = matches.get(type);
+//                if (matchedSpans.containsKey(currentMarker.fbegin)) {
+//                    Marker existMarker = matchedSpans.get(currentMarker.fbegin);
+//                    if (currentMarker.score > existMarker.score || (currentMarker.score == existMarker.score && currentMarker.width > existMarker.width)) {
+//                        matchedSpans.put(currentMarker.fbegin, currentMarker);
+//                    }
+//                } else {
+//                    matchedSpans.put(currentMarker.fbegin, currentMarker);
+//                }
+//            } else {
+//                matchedSpans.put(currentMarker.fbegin, currentMarker);
+//                matches.put(type, matchedSpans);
+//            }
+//        }
+//    }
 
 }
---
+++ src/test/java/edu/utah/bmi/nlp/RuSH/TestRuSH2.java
@@ -0,0 +1,137 @@
+/*******************************************************************************
+ * Copyright  2016  Department of Biomedical Informatics, University of Utah
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package edu.utah.bmi.nlp.RuSH;
+
+import edu.utah.bmi.nlp.core.Span;
+import edu.utah.bmi.nlp.rush.core.RuSH;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+/**
+ * This rule set are more inclusive that is trying to include all non-whitespace characters in a sentence.
+ *
+ * @Author Jianlin Shi
+ */
+public class TestRuSH2 {
+    private RuSH rush2;
+
+    public static void printDetails(ArrayList<Span> sentences, String input) {
+        for (int i = 0; i < sentences.size(); i++) {
+            Span sentence = sentences.get(i);
+            System.out.println(">"+input.substring(sentence.begin,sentence.end)+"<");
+            System.out.println("assert (sentences.get(" + i + ").begin == " + sentence.begin + " &&" +
+                    " sentences.get(" + i + ").end == " + sentence.end + ");");
+        }
+
+    }
+
+    @BeforeEach
+    public void initiate() {
+        rush2 = new RuSH("conf/rush_rules_v3.xlsx");
+
+//        rush2 = new RuSH(this.getClass().getClassLoader().getResource("mimic.tsv").getPath());
+//        rush2 = new RuSH("conf/rush_rules.xlsx");
+        rush2.fillTextInSpan=true;
+    }
+
+
+    @Test
+    public void test0() throws Exception {
+        String input = "Pain control ON. ";
+        ArrayList<Span> sentences = rush2.segToSentenceSpans(input);
+        input = input.replaceAll("\\n", " ");
+        printDetails(sentences, input);
+
+
+    }
+
+    @Test
+    public void test1() throws Exception {
+        String input = "Can Mr. K check it. Look\n good.\n";
+        ArrayList<Span> sentences = rush2.segToSentenceSpans(input);
+        input = input.replaceAll("\\n", " ");
+        printDetails(sentences, input);
+        assert (sentences.get(0).begin == 0 && sentences.get(0).end == 19);
+        assert (sentences.get(1).begin == 20 && sentences.get(1).end == 31);
+
+    }
+
+    @Test
+    public void test2() {
+        String input = "S/p C6-7 ACDF. No urgent events overnight. Pain control ON. ";
+        ArrayList<Span> sentences = rush2.segToSentenceSpans(input);
+        input = input.replaceAll("\n", " ");
+        printDetails(sentences, input);
+        assert (sentences.get(0).begin == 0 && sentences.get(0).end == 14);
+        assert (sentences.get(1).begin == 15 && sentences.get(1).end == 42);
+        assert (sentences.get(2).begin == 43 && sentences.get(2).end == 59);
+
+    }
+
+    @Test
+    public void test3() {
+        String input = " •  Coagulopathy (HCC)    \n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                " •  Hepatic encephalopathy (HCC)    \n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                " •  Hepatorenal syndrome (HCC)    \n" +
+                "\n";
+        ArrayList<Span> sentences = rush2.segToSentenceSpans(input);
+        input = input.replaceAll("\n", " ");
+        printDetails(sentences, input);
+        assert (sentences.get(0).begin == 1 && sentences.get(0).end == 22);
+        assert (sentences.get(1).begin == 31 && sentences.get(1).end == 62);
+        assert (sentences.get(2).begin == 71 && sentences.get(2).end == 100);
+
+    }
+
+
+    @Test
+    public void test4() {
+        String input = "Delirium - ";
+        ArrayList<Span> sentences = rush2.segToSentenceSpans(input);
+        input = input.replaceAll("\n", " ");
+        printDetails(sentences, input);
+        assert (sentences.get(0).begin == 0 && sentences.get(0).end == 10);
+    }
+
+    @Test
+    public void test5() {
+        String input = "The patient complained about the TIA \n\n No memory issues. \"I \n\nOrdered the MRI scan.- ";
+        ArrayList<Span> sentences = rush2.segToSentenceSpans(input);
+        input = input.replaceAll("\n", " ");
+        printDetails(sentences, input);
+        System.out.println(sentences);
+    }
+
+    @Test
+    public void test6() {
+        String input = "Check it. And it is.";
+        ArrayList<Span> sentences = rush2.segToSentenceSpans(input);
+        input = input.replaceAll("\n", " ");
+        printDetails(sentences, input);
+        System.out.println(sentences);
+    }
+
+
+
+}
